7ab58063d86ae2d9a5f700b0b88c62f51d90a7a9
Logapriya <slogaapriya@gmail.com>
2023-06-02 19:47:37 +0530

    Added Icons

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Cart } from "../Cart/Cart";
 import "./NavBar.css"
+import { CartContext } from "../../Contexts/CartContext";
+import { WishListContext } from "../../Contexts/WishListContext";
+import {FaShoppingCart} from "react-icons/fa"
+import {GiLargeDress} from "react-icons/gi"
+import {BsHeartFill} from "react-icons/bs"
+import {CgProfile} from "react-icons/cg"
+import {IoHomeSharp} from "react-icons/io5"
 export function Navbar() {
+  const {cartItem}=useContext(CartContext)
+  const {wishList}=useContext(WishListContext)
+  //const {wishList}=useContext(WishListContext)
   return <nav className="Navigation-Container">
     <div className="nav-brand">Trend Corner</div>
     <div>
@@ -10,18 +20,19 @@ export function Navbar() {
     <div className="nav-links">
       <ul className="nav-items">
         <li className="nav-item">
-          <Link className="Link" to="/" >Home</Link>
+          <Link className="Link" to="/" ><IoHomeSharp/></Link>
         </li>
         <li className="nav-item">
-          <Link className="Link" to="/products">Products</Link>
+          <Link className="Link" to="/products"><GiLargeDress/></Link>
         </li>
         <li className="nav-item">
-          <Link className="Link" to="/wishlist">WishList</Link></li>
-        <li className="nav-item">
-          <Link className="Link" to="/cart">Cart</Link>
+          <Link className="Link" to="/wishlist"><BsHeartFill/>{wishList.length}</Link>
         </li>
         <li className="nav-item">
-          <Link className="Link" to="/login">Login</Link>
+          <Link className="Link" to="/cart"><FaShoppingCart/>{cartItem.length}</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="Link" to="/login"><CgProfile/></Link>
         </li>
       </ul>
     </div>
