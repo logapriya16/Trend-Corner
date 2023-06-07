4b753825ede8963027754453888e22bf6844f66d
Logapriya <slogaapriya@gmail.com>
2023-06-07 18:05:58 +0530

    Toastify added

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,95 +1,178 @@
-import React from "react"
-import { useContext, useEffect } from "react"
-import { CartContext } from "../../Contexts/CartContext"
-import "./Cart.css"
-//import { BsCart3 } from "react-icons/bs"
-//import { useNavigate } from "react-router-dom"
-import { WishListContext } from "../../Contexts/WishListContext"
+import React, { useState } from "react";
+import { useContext, useEffect } from "react";
+import { CartContext } from "../../Contexts/CartContext";
+import "./Cart.css";
+import { WishListContext } from "../../Contexts/WishListContext";
+import { CartPriceDetails } from "../../Components/CartPriceDetails";
+import { toast } from "react-toastify";
 export function Cart() {
-    //const navigate = useNavigate()
-    const { GetCartItems, ChangeQuantity, cartItem, DeleteFromCart, TotalPrice } = useContext(CartContext)
-    const { AddToWishList,wishList } = useContext(WishListContext)
-    useEffect(() => { GetCartItems() }, [])
-    return <div >
-        <h1>Cart Page</h1>
-        <ul className="cart-container" type="none">
-            {cartItem.map((item) => {
-                const IsWishListed = wishList.find((wishListItem) => wishListItem._id === item._id)
-                return <div className="cart-product-container">
-                    <li className="cart-contents">
-                        <div className="cart-product-image">
-                            <img className="cart-product-image" src={item.image} alt="product-img" />
-                        </div>
-                        <div className="cart-product-details">
-                            <div className="cart-product-name">
-                                {item.name}
-                            </div>
-                            <div className="cart-product-stock">
-                                <p>{item.instock ? "instock" : "out of stock"}</p>
-                            </div>
-                            <div className="cart-product-occation">
-                                {item.occation}
-                            </div>
-                            <div className="cart-product-size">
-                                <p>Size : {item.size}</p>
-                            </div>
-                            <div className="cart-product-price">
-                                <p className="cart-product-discount-price"> ₹{item.price - (item.price * (item.discount / 100))}</p>
-                                <p className="cart-product-original-price"> ₹{item.price}</p>
-                                <p className="cart-product-dis-percentage">{item.discount}%off</p>
-                            </div>
-                            <div className="cart-product-quantity">
-                                <button onClick={() => ChangeQuantity(item._id, "increment")}>+</button>
-                                <p>{item.qty}</p>
-                                <button onClick={() => ChangeQuantity(item._id, "decrement")} disabled={item.qty > 1 ? false : true}>-</button>
-                            </div>
-                        </div>
-                    </li>
-                    <hr />
-                    <div className="cart-button">
-                        
-                        <div>
-                            {IsWishListed ?
-                                <button className="cart-primary-button"
-                                    onClick={() => AddToWishList(item, false)}>
-                                    Remove from Wishlist
-                                </button>:
-                                <button className="cart-primary-button"
-                                onClick={() => AddToWishList(item, true)}>
-                                Move To Wishlist
-                            </button> 
-                            }
-                        </div>
-                        <button className="cart-primary-button"
-                            onClick={() => DeleteFromCart(item._id)}>
-                            Remove Form Cart
-                        </button>
-                    </div>
+  //const navigate = useNavigate()
+  const {
+    GetCartItems,
+    ChangeQuantity,
+    cartItem,
+    DeleteFromCart,
+    TotalPrice,
+  } = useContext(CartContext);
+  const { AddToWishList, wishList } = useContext(WishListContext);
+  const [coupon, UseCoupon] = useState(false);
+
+  useEffect(() => {
+    GetCartItems();
+  }, []);
+  return (
+    <div className="cart-page">
+      <ul className="cart-container" type="none">
+        {cartItem.map((item) => {
+          const IsWishListed = wishList.find(
+            (wishListItem) => wishListItem._id === item._id
+          );
+          return (
+            <div className="cart-product-container">
+              <li className="cart-contents">
+                <div className="cart-product-image">
+                  <img
+                    className="cart-product-image"
+                    src={item.image}
+                    alt="product-img"
+                  />
                 </div>
-            })}
-        </ul>
-        <div className="CheckOut-Container">
-            <div className="coupon-container">
-                Have A Coupon?
-                <button className="cart-primary-button">
-                    Apply
-                </button>
-                <div>
-                    <hr /><h3>PRICE DETAILS</h3><hr />
-                    <p>Price({cartItem.length}items){TotalPrice}<br />
-                        <p></p> Discount {cartItem.map((item) =>
-                            item.price * (item.discount / 100))}<br />
-                        Delivery Charges FREE<br />
-                        Coupon Discount { }
-                        <hr />
-                        TOTAL AMOUNT {TotalPrice}
-                        <hr />
-                        You will save { } on this order
-                        <br />
-                        <button className="primary-button"> Checkout</button>
+                <div className="cart-product-details">
+                  <div className="cart-product-name">{item.name}</div>
+                  <div className="cart-product-occation">{item.occation}</div>
+                  <div className="cart-product-size">
+                    <p>Size : {item.size}</p>
+                  </div>
+                  <div className="cart-product-price">
+                    <p className="cart-product-discount-price">
+                      {" "}
+                      ₹{item.price - item.price * (item.discount / 100)}
                     </p>
+                    <p className="cart-product-original-price">
+                      {" "}
+                      ₹{item.price}
+                    </p>
+                    <p className="cart-product-dis-percentage">
+                      {item.discount}%off
+                    </p>
+                  </div>
+                  <div className="cart-product-quantity">
+                    <button
+                      className="cart-qty-btn"
+                      onClick={() => {
+                        ChangeQuantity(item._id, "increment");
+                        toast.success("Quanity Increased", {
+                          position: "bottom-right",
+                          autoClose: 5000,
+                          hideProgressBar: false,
+                          closeOnClick: true,
+                          pauseOnHover: true,
+                          draggable: true,
+                          progress: undefined,
+                          theme: "light",
+                        });
+                      }}
+                    >
+                      +
+                    </button>
+                    <p className="cart-product-qty">{item.qty}</p>
+                    <button
+                      className="cart-qty-btn"
+                      onClick={() => {
+                        ChangeQuantity(item._id, "decrement");
+                        toast.warning("Quanity Decresed", {
+                          position: "bottom-right",
+                          autoClose: 5000,
+                          hideProgressBar: false,
+                          closeOnClick: true,
+                          pauseOnHover: true,
+                          draggable: true,
+                          progress: undefined,
+                          theme: "light",
+                        });
+                      }}
+                      disabled={item.qty > 1 ? false : true}
+                    >
+                      -
+                    </button>
+                  </div>
                 </div>
+              </li>
+              <hr />
+              <div className="cart-button">
+                <div>
+                  {IsWishListed ? (
+                    <button
+                      className="cart-primary-button"
+                      onClick={() =>{AddToWishList(item, false);
+                        toast.warning("Item Removed From WishList", {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                      });} }
+                    >
+                      Remove from Wishlist
+                    </button>
+                  ) : (
+                    <button
+                      className="cart-primary-button"
+                      onClick={() =>{AddToWishList(item, true);
+                        toast.success("Item Added To WishList", {
+                            position: "bottom-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                          });
+                    } }
+                    >
+                      Add To Wishlist
+                    </button>
+                  )}
+                </div>
+                <button
+                  className="cart-primary-button"
+                  onClick={() =>{ 
+                    toast.warning("Item Removed From Cart", {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                      });
+                  DeleteFromCart(item._id)}}>
+                  Remove Form Cart
+                </button>
+              </div>
             </div>
+          );
+        })}
+      </ul>
+      <div className="CheckOut-Container">
+        <div style={{ textAlign: "center", paddingBottom: "-1rem" }}>
+          <span>Have A Coupon?</span>
+          <button
+            className="cart-primary-button"
+            disabled={coupon}
+            onClick={() => UseCoupon(true)}
+          >
+            Apply
+          </button>
         </div>
+
+        <CartPriceDetails />
+      </div>
     </div>
+  );
 }
--- src/Pages/FilterComponent/FilterComponent.jsx
+++ src/Pages/FilterComponent/FilterComponent.jsx
@@ -1,128 +1,247 @@
-import React from "react"
-import "./FilterComponent.css"
+import React, { useContext } from "react";
+import "./FilterComponent.css";
+import { FilterContext } from "../../Contexts/FiltersContext";
+
 export const FilterComponent = () => {
-const filterByPriceRange=(range)=>{
-return <div></div>
-}
-const filterByCategory=(category)=>{
-    return <div></div>
-}
-const filterByRating=(rating)=>{
-    return <div>
-        {}
+  const {
+    HandleCategory,
+    HandleRating,
+    HandleOccation,
+    HandleRange,
+    HandleSize,
+    HandleSort,
+  } = useContext(FilterContext);
+  return (
+    <div className="filters">
+      <div className="filter-head">
+        <p>
+          <b>Filters</b>
+        </p>
+        <p>Clear</p>
+      </div>
+
+      <div className="filter-price-range">
+        <label for="price-range">
+          <h4>Price</h4>
+        </label>
+        <input
+          type="range"
+          title="price"
+          id="price-range"
+          min={0}
+          max={1000}
+          step={100}
+          onChange={(e) => HandleRange(e)}
+        />
+      </div>
+
+      <div className="filter-categories">
+        <h4>Category</h4>
+        <div className="filter-category">
+          <input
+            id="gowns"
+            type="checkbox"
+            value="gowns"
+            title="gowns"
+            onClick={(e) => {
+              HandleCategory(e.target.value);
+            }}
+          />
+          <label for="gowns">Gowns</label>
+        </div>
+        <div className="filter-category">
+          <input
+            id="jumpsuits"
+            type="checkbox"
+            value="jumpsuits"
+            className="category"
+            title="jumpsuits"
+            onClick={(e) => {
+              HandleCategory(e.target.value);
+            }}
+          />
+          <label for="jumpsuits">Jumpsuits</label>
+        </div>
+        <div className="filter-category">
+          <input
+            id="t-shirts"
+            type="checkbox"
+            value="t-shirts"
+            className="category"
+            title="t-shirts"
+            onClick={(e) => {
+              HandleCategory(e.target.value);
+            }}
+          />
+          <label for="t-shirts">T Shirts</label>
+        </div>
+
+        <div className="filter-category">
+          <input
+            id="kurtha"
+            type="checkbox"
+            value="kurtha"
+            className="category"
+            title="kurtha"
+            onClick={(e) => {
+              HandleCategory(e.target.value);
+            }}
+          />
+          <label for="kurtha">Kurtha</label>
+        </div>
+      </div>
+
+      <div className="filter-rating">
+        <h4>Rating</h4>
+        <div className="filter-rate">
+          <input
+            name="rating"
+            id="1&<"
+            title="1&<"
+            type="radio"
+            value="1"
+            min="0"
+            max="10000"
+            onClick={(e) => HandleRating(e)}
+          />
+          <label for="1&<"> 1 stars & above</label>
+        </div>
+        <div className="filter-rate">
+          <input
+            name="rating"
+            id="2&<"
+            type="radio"
+            title="2&<"
+            value="2"
+            onClick={(e) => HandleRating(e)}
+          />
+          <label for="2&<">2 stars & above</label>
+        </div>
+        <div className="filter-rate">
+          <input
+            name="rating"
+            type="radio"
+            title="3&<"
+            id="3&<"
+            value="3"
+            onClick={(e) => HandleRating(e)}
+          />
+          <label for="3&<">3 stars & above</label>
+        </div>
+        <div className="filter-rate">
+          <input
+            name="rating"
+            type="radio"
+            title="4&<"
+            id="4&<"
+            value="4"
+            onClick={(e) => HandleRating(e)}
+          />
+          <label for="4&<">4 stars & above</label>
+        </div>
+      </div>
+
+      <div className="filter-occations">
+        <h4>Occation</h4>
+        <div className="filter-occation">
+          <input
+            type="checkbox"
+            id="ceremony"
+            value="ceremony"
+            onClick={(e) => HandleOccation(e)}
+          />
+          <label htmlFor="ceremony">Ceremony</label>
+        </div>
+        <div className="filter-occation">
+          <input
+            type="checkbox"
+            id="casual"
+            value="casual"
+            onClick={(e) => HandleOccation(e)}
+          />
+          <label htmlFor="casual">Casual</label>
+        </div>
+      </div>
+
+      <div className="filter-sizes">
+        <h4>Size</h4>
+        <div className="filter-size">
+          <input
+            type="checkbox"
+            name="Small"
+            id="S"
+            value="S"
+            onClick={(e) => HandleSize(e)}
+          />
+          <label htmlFor="S">Small</label>
+        </div>
+        <div className="filter-size">
+          <input
+            type="checkbox"
+            name="Large"
+            id="L"
+            value="L"
+            onClick={(e) => HandleSize(e)}
+          />
+          <label htmlFor="L">Large</label>
+        </div>
+        <div className="filter-size">
+          <input
+            type="checkbox"
+            name="Medium"
+            id="M"
+            value="M"
+            onClick={(e) => HandleSize(e)}
+          />
+          <label htmlFor="M">Medium</label>
+        </div>
+        <div className="filter-size">
+          <input
+            type="checkbox"
+            name="ExtraLarge"
+            id="XL"
+            value="XL"
+            onClick={(e) => HandleSize(e)}
+          />
+          <label htmlFor="XL">Extra Large</label>
+        </div>
+        <div className="filter-size">
+          <input
+            type="checkbox"
+            name="ExtraExtraLarge"
+            id="XXL"
+            value="XXL"
+            onClick={(e) => HandleSize(e)}
+          />
+          <label htmlFor="XXL">Extra Extra Large</label>
+        </div>
+      </div>
+
+      <div className="filter-price-sort">
+        <h4>Sort By</h4>
+        <div className="filter-price">
+          <label for="LowToHigh">Price-Low to High</label>
+          <input
+            type="radio"
+            title="LowToHigh"
+            name="price"
+            id="LowToHigh"
+            value="asc"
+            onClick={(e) => HandleSort(e.target.value)}
+          />
+        </div>
+        <div className="filter-price">
+          <label for="HighToLow">Price-High to Low</label>
+          <input
+            type="radio"
+            title="HighToLow"
+            name="price"
+            id="HighToLow"
+            value="des"
+            onClick={(e) => HandleSort(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
-} 
-const filterBySize=(size)=>{
-    return <div></div>
-}
-const filterByOccation=(size)=>{
-    return <div></div>
-}
-const filterByPriceSort=(sort)=>{
-    return <div></div>
-}
-return <div className="filters">
-        <div className="filter-head">
-            <p ><b>Filters</b></p>
-            <p>Clear</p>
-        </div>
-        <div className="filter-price-range">
-            <label for="price-range"><h4>Price</h4></label>
-            <input type="range" title="price" id="price-range"  onChange={(e)=>filterByPriceRange(e.target.value)} />
-        </div>
-        <div className="filter-categories">
-            <h4>Category</h4>
-            <div className="filter-category">
-                <input id="gowns" type="checkbox" value="gowns" title="gowns" onChange={(e)=>filterByCategory(e.target.value)} />
-                <label for="gowns">Gowns</label>
-            </div>
-            <div className="filter-category">
-                <input id="jumpsuits" type="checkbox" value="jumpsuits" className="category" title="jumpsuits" onChange={(e)=>filterByCategory(e.target.value)} />
-                <label for="jumpsuits">Jumpsuits</label>
-            </div>
-            <div className="filter-category">
-                <input id="t-shirts" type="checkbox" value="t-shirts" className="category" title="t-shirts" onChange={(e)=>filterByCategory(e.target.value)} />
-                <label for="t-shirts">T Shirts</label>
-            </div>
-            <div className="filter-category">
-                <input id="kurtha" type="checkbox" value="kurtha" className="category" title="kurtha" onChange={(e)=>filterByCategory(e.target.value)} />
-                <label for="kurtha">Kurtha</label>
-            </div>
-        </div>
-        <div className="filter-rating">
-            <h4>Rating</h4>
-            <div className="filter-rate">
-                <input name="rating" id="1&<" title="1&<" type="radio" value="1" onChange={(e)=>filterByRating(e.target.value)} />
-                <label for="1&<"> 1 stars & above</label>
-
-            </div>
-            <div className="filter-rate">
-                <input name="rating" id="2&<" type="radio" title="2&<" value="2" onChange={(e)=>filterByRating(e.target.value)} />
-                <label for="2&<">2 stars & above</label>
-
-            </div>
-            <div className="filter-rate">
-
-                <input name="rating" type="radio" title="3&<" id="3&<" value="3" onChange={(e)=>filterByRating(e.target.value)} />
-                <label for="3&<">3 stars & above</label>
-
-            </div>
-            <div className="filter-rate">
-                <input name="rating" type="radio" title="4&<" id="4&<" value="4" onChange={(e)=>filterByRating(e.target.value)} />
-                <label for="4&<">4 stars & above</label>
-
-            </div>
-        </div>
-
-
-        <div className="filter-occations">
-            <h4>Occation</h4>
-            <div className="filter-occation">
-                <input type="checkbox" id="ceremony" value="ceremony" onChange={(e)=>filterByOccation(e.target.value)} />
-                <label htmlFor="ceremony">Ceremony</label>
-            </div>
-            <div className="filter-occation">
-                <input type="checkbox" id="casual" value="casual" onChange={(e)=>filterByOccation(e.target.value)} />
-                <label htmlFor="casual">Casual</label>
-            </div>
-
-        </div>
-        <div className="filter-sizes">
-            <h4>Size</h4>
-            <div className="filter-size">
-                <input type="checkbox" name="Small" id="S"  value="S" onChange={(e)=>filterBySize(e.target.value)}/>
-                <label htmlFor="S">Small</label>
-            </div>
-            <div className="filter-size">
-                <input type="checkbox" name="Large" id="L" value="L"  onChange={(e)=>filterBySize(e.target.value)} />
-                <label htmlFor="L">Large</label>
-            </div>
-            <div className="filter-size">
-                <input type="checkbox" name="Medium" id="M"  value="M"  onChange={(e)=>filterBySize(e.target.value)} />
-                <label htmlFor="M">Medium</label>
-            </div>
-            <div className="filter-size">
-                <input type="checkbox" name="ExtraLarge" id="XL" value="XL"  onChange={(e)=>filterBySize(e.target.value)} />
-                <label htmlFor="XL">Extra Large</label>
-            </div>
-            <div className="filter-size">
-                <input type="checkbox" name="ExtraExtraLarge" id="XXL" value="XXL"  onChange={(e)=>filterBySize(e.target.value)} />
-                <label htmlFor="XXL">Extra Extra Large</label>
-            </div>
-            
-        </div>
-        <div className="filter-price-sort">
-            <h4>Sort By</h4>
-            <div className="filter-price">
-                <label for="LowToHigh">Price-Low to High</label>
-                <input type="radio" title="LowToHigh" name="price" id="LowToHigh" value="Low"  onChange={(e)=>filterByPriceSort(e.target.value)} />
-            </div>
-            <div className="filter-price" onChange={(e)=>filterByPriceSort(e.target.value)} >
-                <label for="HighToLow">Price-High to Low</label>
-                <input type="radio" title="HighToLow" name="price" id="HighToLow" value="High" onChange={(e)=>filterByPriceSort(e.target.value)} />
-            </div>
-
-        </div>
-    </div>
-
-}
+  );
+};
